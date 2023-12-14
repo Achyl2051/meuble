e5e636e42d99c0075dfc038b7e8d178f5ceb3999
RASOLOARISON Tahiry Johary <118507545+Achyl2051@users.noreply.github.com>
2023-12-14 10:40:06 +0300

    update base prediction

--- base.sql
+++ base.sql
@@ -1,5 +1,15 @@
+DROP DATABASE meuble;
+
 CREATE DATABASE meuble;
 \c meuble
+
+CREATE TABLE users(
+    id serial PRIMARY KEY,
+    password TEXT,
+    pseudo TEXT,
+    email TEXT
+);
+INSERT INTO users(password,pseudo,email) VALUES('root','achyl','achyl@root.mg');
 
 CREATE TABLE style(
     idStyle serial PRIMARY KEY,
@@ -9,14 +19,24 @@ INSERT INTO style(nom) VALUES('royal');
 INSERT INTO style(nom) VALUES('scandinave');
 INSERT INTO style(nom) VALUES('oriental');
 
-CREATE TABLE materiel(
-    idMateriel serial PRIMARY KEY,
+CREATE TABLE unite(
+    idUnite serial PRIMARY KEY,
     nom TEXT
 );
-INSERT INTO materiel(nom) VALUES('hazo');
-INSERT INTO materiel(nom) VALUES('plastique');
-INSERT INTO materiel(nom) VALUES('marbre');
-INSERT INTO materiel(nom) VALUES('cuire');
+INSERT INTO unite(nom) VALUES('unites');
+INSERT INTO unite(nom) VALUES('kg');
+INSERT INTO unite(nom) VALUES('litres');
+
+CREATE TABLE materiel(
+    idMateriel serial PRIMARY KEY,
+    idUnite int,
+    nom TEXT,
+    FOREIGN KEY (idUnite) REFERENCES unite(idUnite)
+);
+INSERT INTO materiel(nom) VALUES('hazo','2');
+INSERT INTO materiel(nom) VALUES('plastique','2');
+INSERT INTO materiel(nom) VALUES('marbre','2');
+INSERT INTO materiel(nom) VALUES('cuire','2');
 
 CREATE TABLE detailStyle(
     idDetail serial PRIMARY KEY,
@@ -26,10 +46,39 @@ CREATE TABLE detailStyle(
     FOREIGN KEY (idMateriel) REFERENCES materiel(idMateriel)
 );
 
-CREATE TABLE users(
-    id serial PRIMARY KEY,
-    password TEXT,
-    pseudo TEXT,
-    email TEXT
+CREATE TABLE categorie(
+    idCategorie serial PRIMARY KEY,
+    nom TEXT
 );
-INSERT INTO users(password,pseudo,email) VALUES('root','achyl','achyl@root.mg');
+INSERT INTO categorie(nom) VALUES('seza');
+INSERT INTO categorie(nom) VALUES('table');
+INSERT INTO categorie(nom) VALUES('fandriana');
+
+CREATE TABLE volume(
+    idVolume serial PRIMARY KEY,
+    nom TEXT,
+    valeurMin double precision,
+    valeurMax double precision
+);
+INSERT INTO volume(nom,valeurMin,valeurMax) VALUES('PM',1,10);
+INSERT INTO volume(nom,valeurMin,valeurMax) VALUES('Medium',11,20);
+INSERT INTO volume(nom,valeurMin,valeurMax) VALUES('GM',21,30);
+
+CREATE TABLE meuble(
+    idMeuble serial PRIMARY KEY,
+    idStyle int,
+    idCategorie int,
+    idVolume int,
+    nom VARCHAR(255),
+    prix double precision, 
+    FOREIGN KEY (idStyle) REFERENCES style(idStyle),
+    FOREIGN KEY (idCategorie) REFERENCES categorie(idCategorie),
+    FOREIGN KEY (idVolume) REFERENCES volume(idVolume)
+);
+
+CREATE TABLE meubleMateriel(
+    idMM serial PRIMARY KEY,
+    idMeuble int,
+    idMateriel int,
+    quantite double precision
+);
